f035d50bf7827afd5eafe1a8df0767f9a46e22d1
André Donadon <andre.donadon@outlook.com>
2021-05-11 16:46:52 -0300

    5.3

---
+++ src/main/java/br/com/escola/aplicacao/indicacao/EnviarEmailIndicacao.java
@@ -0,0 +1,9 @@
+package br.com.escola.aplicacao.indicacao;
+
+import br.com.escola.dominio.aluno.Aluno;
+
+public interface EnviarEmailIndicacao {
+
+    void enviarPara(Aluno indicado);
+
+}
---
+++ src/main/java/br/com/escola/infra/indicacao/EnviarEmailIndicacaoJavaMail.java
@@ -0,0 +1,12 @@
+package br.com.escola.infra.indicacao;
+
+import br.com.escola.aplicacao.indicacao.EnviarEmailIndicacao;
+import br.com.escola.dominio.aluno.Aluno;
+
+public class EnviarEmailIndicacaoJavaMail implements EnviarEmailIndicacao {
+
+    @Override
+    public void enviarPara(Aluno indicado) {
+
+    }
+}
